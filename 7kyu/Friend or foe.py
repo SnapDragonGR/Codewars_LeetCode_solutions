d7e9659cc14fc9e3acb26885a0659525f1a35caf
SnapDragonGR <thesanegleb@gmail.com>
2024-08-17 12:15:07 +0200

    Practice use of regular expressions with a bit of cheating

---
+++ 7kyu/Friend or foe.py
@@ -0,0 +1,33 @@
+"""
+Make a program that
+filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+Otherwise, you can be sure he's not...
+
+Input = {"Ryan", "Kieran", "Jason", "Yous"}
+Output = {"Ryan", "Yous"}
+
+Input = {"Peter", "Stephen", "Joe"}
+Output = {}
+Input strings will only contain letters.
+Note: keep the original order of the names in the output.
+"""
+
+# Solution (with a bit of cheating):
+
+
+import re
+
+
+def friend(x):
+    pattern = re.compile(r'\b[A-Za-z]{4}\b')
+    friends = []
+
+    for element in x:
+        words = re.findall(r'\b[A-Za-z]+\b', element)
+        for name in words:
+            if pattern.fullmatch(name) and name != "Cool":
+                friends.append(name)
+
+    return friends
